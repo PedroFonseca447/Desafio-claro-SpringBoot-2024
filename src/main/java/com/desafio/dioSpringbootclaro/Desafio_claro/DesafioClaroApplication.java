9854a8f3473cf4443b0f719a772fd5394b4f39ac
Pedro Fonseca <pedrofonseca447@gmail.com>
2024-11-04 19:28:15 -0300

    dando inicio ao projeto

---
+++ src/main/java/com/desafio/dioSpringbootclaro/Desafio_claro/DesafioClaroApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.dioSpringbootclaro.Desafio_claro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioClaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioClaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/dioSpringbootclaro/Desafio_claro/DesafioClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.dioSpringbootclaro.Desafio_claro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioClaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
