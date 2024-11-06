c80ef8a3783769062893d2e4f83d48ee76b0a703
Pedro Fonseca <pedrofonseca447@gmail.com>
2024-11-06 02:20:36 -0300

    publicado

--- src/main/java/com/desafio/dioSpringbootclaro/Desafio_claro/DesafioClaroApplication.java
+++ src/main/java/com/desafio/dioSpringbootclaro/Desafio_claro/DesafioClaroApplication.java
@@ -3,11 +3,15 @@ package com.desafio.dioSpringbootclaro.Desafio_claro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class DesafioClaroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioClaroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioClaroApplication.class, args);
+    }
 
 }
